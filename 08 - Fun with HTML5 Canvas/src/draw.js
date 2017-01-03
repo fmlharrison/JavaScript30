f087885756224c03365de18f5e2fed173e8ac313
Felix Harrison <fmlharrison@gmail.com>
2017-01-03 14:17:40 +0000

    user can change colour of line

--- 08 - Fun with HTML5 Canvas/src/draw.js
+++ 08 - Fun with HTML5 Canvas/src/draw.js
@@ -1,8 +1,12 @@
 /*jshint esversion: 6*/
 const colorCanvas = document.querySelector('#color-box');
 const sliders = document.querySelectorAll('.color-controls input');
+const color = document.querySelector('#hue');
+const saturation = document.querySelector('#saturation');
+const lightness = document.querySelector('#lightness');
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
@@ -13,19 +17,16 @@ ctx.lineWidth = 25;
 let isDrawing = false; //this is a dummy variable
 let lastX = 0;
 let lastY = 0;
-let hue = 0;
 let direction = true;
 
 function draw(e) {
   if (!isDrawing) return;
-  console.log(e);
-  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.strokeStyle = `hsl(${color.value}, ${saturation.value}%, ${lightness.value}%)`;
   ctx.beginPath(); //starts the line drawing
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY]; //This is restructuring an array.
-  hue++;
 
   if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
     direction = !direction;
@@ -37,9 +38,16 @@ function draw(e) {
   }
 }
 
-function changeColour() {
+function changeColour(e) {
+  console.log(e.target);
   const suffix = this.dataset.percent || '';
-  document.documentElement.style.setProperty(`--base`, `hsl(${this.value}, 100%, 50%)`);
+  if (e.target === color) {
+    document.documentElement.style.setProperty(`--hue`, this.value);
+  } else if (e.target === saturation) {
+    document.documentElement.style.setProperty(`--saturation`, this.value + suffix);
+  } else if (e.target === lightness) {
+    document.documentElement.style.setProperty(`--lightness`, this.value + suffix);
+  }
 }
 
 canvas.addEventListener('mousedown', (e) => {
@@ -51,4 +59,8 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
-sliders.forEach(slider => slider.addEventListener('change', changeColour));
+color.addEventListener('change', changeColour);
+saturation.addEventListener('change', changeColour);
+lightness.addEventListener('change', changeColour);
+
+// sliders.forEach(slider => slider.addEventListener('change', changeColour));
