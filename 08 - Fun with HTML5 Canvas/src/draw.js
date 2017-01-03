6fd3a090c5aa37253e681e78758ee82c69d30efe
Felix Harrison <fmlharrison@gmail.com>
2017-01-03 15:07:01 +0000

    can change the hight of the line and clear the canvas

--- 08 - Fun with HTML5 Canvas/src/draw.js
+++ 08 - Fun with HTML5 Canvas/src/draw.js
@@ -4,6 +4,8 @@ const sliders = document.querySelectorAll('.color-controls input');
 const color = document.querySelector('#hue');
 const saturation = document.querySelector('#saturation');
 const lightness = document.querySelector('#lightness');
+const lineWidth = document.querySelector('#width');
+const clear = document.querySelector('#clear');
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 
@@ -12,7 +14,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 25;
+ctx.lineWidth = 10;
 
 let isDrawing = false; //this is a dummy variable
 let lastX = 0;
@@ -22,25 +24,16 @@ let direction = true;
 function draw(e) {
   if (!isDrawing) return;
   ctx.strokeStyle = `hsl(${color.value}, ${saturation.value}%, ${lightness.value}%)`;
+  ctx.lineWidth = lineWidth.value;
   ctx.beginPath(); //starts the line drawing
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY]; //This is restructuring an array.
-
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-    direction = !direction;
-  }
-  if (direction) {
-    ctx.lineWidth++;
-  } else {
-    ctx.lineWidth--;
-  }
 }
 
 function changeColour(e) {
-  console.log(e.target);
-  const suffix = this.dataset.percent || '';
+  let suffix = this.dataset.percent || '';
   if (e.target === color) {
     document.documentElement.style.setProperty(`--hue`, this.value);
   } else if (e.target === saturation) {
@@ -48,6 +41,16 @@ function changeColour(e) {
   } else if (e.target === lightness) {
     document.documentElement.style.setProperty(`--lightness`, this.value + suffix);
   }
+}
+
+function changeWidth() {
+  let suffix = this.dataset.sizing;
+  document.documentElement.style.setProperty("--line-width", this.value + suffix);
+}
+
+function clearCanvas(e) {
+  console.log(e);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 canvas.addEventListener('mousedown', (e) => {
@@ -60,7 +63,13 @@ canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
 color.addEventListener('change', changeColour);
+color.addEventListener('mousemove', changeColour);
 saturation.addEventListener('change', changeColour);
+saturation.addEventListener('mousemove', changeColour);
 lightness.addEventListener('change', changeColour);
+lightness.addEventListener('mousemove', changeColour);
 
-// sliders.forEach(slider => slider.addEventListener('change', changeColour));
+lineWidth.addEventListener('click', changeWidth);
+lineWidth.addEventListener('change', changeWidth);
+
+clear.addEventListener('click', clearCanvas);
